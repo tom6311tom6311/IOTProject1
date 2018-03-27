21fe09797adfc7eb97918b47a3b2a35685d872fd
tomHuang <tom.huang@positivegrid.com>
2018-03-27 16:12:48 +0800

    feat: compute and upload diff image
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- PiSensorFunction/sensor.py
+++ PiSensorFunction/sensor.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
-import datetime
 import os
 import re
 import shutil
+import numpy as np
 from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult, IoTHubError
+from scipy.misc import *
 
 # App constants
 CONNECTION_STRING = 'HostName=tom6311tom6311-demo.azure-devices.net;DeviceId=rpi-hardware-test;SharedAccessKey=Ta0czoj3OpsoGlzTbPy68vCvsFqmHCQDJnGK5P5se2I='
@@ -15,6 +16,11 @@ EVENT_FAILED = "failed"
 SOUND_SENSOR_CHANNEL = 12
 IMAGE_FORMAT = '.png'
 PHOTO_FOLDER = 'photos/'
+REGULAR_SHOT_INTERVAL = 60
+DIFF_TOLORANCE = 2
+
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
 
 def iothub_client_init():
   client = IoTHubClient(CONNECTION_STRING, CONNECTION_PROTOCOL)
@@ -30,16 +36,26 @@ def upload_photo(file_name):
   print ( "" )
   print ( "Uploading photo..." )
 
+def compute_diff_photo(file_name, prev_file_name):
+  abnormal_img = imread(PHOTO_FOLDER + file_name)
+  normal_img = imread(PHOTO_FOLDER + prev_file_name)
+  diff_img_mask = sigmoid(np.abs(abnormal_img - normal_img) - DIFF_TOLORANCE)
+  diff_img = np.multiply(diff_img_mask, abnormal_img)
+  diff_photo_name = PHOTO_FOLDER + 'diff_' + file_name
+  imsave(diff_photo_name, diff_img)
+  return diff_photo_name
+
 def abnormal_sound_callback(channel):
   if GPIO.input(channel)== GPIO.LOW:
     # print("Sound low!")
     pass
   else:
     curr_time = time.strftime("%Y.%m.%d-%H.%M.%S")
-    photo_name = curr_time + IMAGE_FORMAT
+    photo_name = curr_time + '_abnormal' + IMAGE_FORMAT
     print("A loud sound occurred at " + curr_time)
     take_photo(photo_name)
-    upload_photo(photo_name)
+    diff_photo_name = compute_diff_photo(photo_name, prev_regular_photo_name)
+    upload_photo(diff_photo_name)
 
 def photo_upload_callback(result, user_context):
   if str(result) == 'OK':
@@ -65,7 +81,13 @@ if __name__ == "__main__":
     client = iothub_client_init()
     GPIO.add_event_detect(SOUND_SENSOR_CHANNEL, GPIO.BOTH, bouncetime=300)
     GPIO.add_event_callback(SOUND_SENSOR_CHANNEL, abnormal_sound_callback)
+    start_time = int(time.time())
     while True:
+      if (int(time.time()) - start_time) % REGULAR_SHOT_INTERVAL == 0:
+        curr_time = time.strftime("%Y.%m.%d-%H.%M.%S")
+        print("Take a regular shot: " + curr_time)
+        prev_regular_photo_name = curr_time + IMAGE_FORMAT
+        take_photo(prev_regular_photo_name)
       time.sleep(1)
   except IoTHubError as iothub_error:
     print ( "Unexpected error %s from IoTHub" % iothub_error )
